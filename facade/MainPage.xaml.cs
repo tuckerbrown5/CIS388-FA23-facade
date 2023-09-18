c1ee39d2a4e7c6fcec36a098756c3485d5c27914
Kenan Casey <kenancasey@gmail.com>
2023-09-18 10:38:05 -0500

    Work from Monday

--- facade/MainPage.xaml.cs
+++ facade/MainPage.xaml.cs
@@ -11,6 +11,9 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+		BindingContext = new MainPageViewModel();
+
 	}
 
     async void Button_Clicked(object sender, EventArgs e)
---
+++ facade/MainPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace facade
+{
+	public partial class MainPageViewModel: ObservableObject 
+	{
+		[ObservableProperty]
+		private string secretColor;
+
+		[ObservableProperty]
+		private string currentGuess;
+
+		public ObservableCollection<string> Guesses { get; set; }
+
+		//public string SecretColor { get; set; }
+
+		public MainPageViewModel()
+		{
+			secretColor = "FACADE";
+			currentGuess = "";
+		}
+
+
+		[RelayCommand]
+		void AddLetter(string letter)
+		{
+			if( CurrentGuess.Length < 6)
+			{
+				CurrentGuess += letter;
+			}
+		}
+
+		void Guess()
+		{
+			// if correct, then go to game over (DidWin=true)
+
+			// else if this is the 6th guess (and it's wrong)
+			// then go to game over (DidWin=false)
+
+
+			// Add this guess to the Guesses
+			Guesses.Add(CurrentGuess);
+
+		}
+
+
+	}
+}
+
